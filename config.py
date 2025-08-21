017f7f40ae189a3fa6bc19a04eebaac5564d10ff
kingjay051 <kingjay1212025@outlook.com>
2025-08-21 17:30:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -75,8 +75,8 @@ START_VIDS = [
 ]
 
 STICKERS = [
-    "CAACAgUAAxkBAjOAo2im83aUwA_FZw-CQ9HxYW0uNLfiAAKAGQAC4Jw5VVkKvcFe0y3iNgQ",
-    "CAACAgUAAxkBAjOAo2im83aUwA_FZw-CQ9HxYW0uNLfiAAKAGQAC4Jw5VVkKvcFe0y3iNgQ"
+    "CAACAgUAAx0Cd6nKUAACASBl_rnalOle6g7qS-ry-aZ1ZpVEnwACgg8AAizLEFfI5wfykoCR4h4E",
+    "CAACAgUAAx0Cd6nKUAACATJl_rsEJOsaaPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ"
 ]
 HELP_IMG_URL = "https://files.catbox.moe/tim0gw.jpg"
 PING_VID_URL = "https://files.catbox.moe/tim0gw.jpg"
